78e5f2c72ebb8edcd65289735e139ddd5dbdbf34
juengeen <77958432+juengeen@users.noreply.github.com>
2021-09-06 11:49:54 +0500

    comments...

---
+++ Value&Types/values.js
@@ -0,0 +1,27 @@
+/*
+JavaScript has built-in types for each these so-called primitive types:
+    1) When you need to do math, you want a number
+    2) When you need to print a value on the screen, you need a string (one or more characters, words, or sentences).
+    3) When you need to make a decision in your program, you need a boolean(true or false)
+
+Values that are included directly in the source code are called literals.
+String literals are surrounded by double quotes ("...") or single quotes ('...').
+The only difference behind this is stylistic preference.
+*/
+
+// Consider
+"I am a string";
+'I am also a string';
+
+42;
+
+true;
+false;
+
+/*
+For all string/number/boolean value types, it's common for programming languages to provide arrays, objects, functions, and more.
+*/
+
+// Converting between types.
+
+// If you need to print it on the screen, you need to convert the value to a string, and in JavaScript this conversion is called "coercion"
