af2d9b4fc47af0d423f57958c6c0c1d6c6877b6a
jackudrew <77958432+jackudrew@users.noreply.github.com>
2021-09-24 11:21:23 +0500

    translation to Russian Language of "for" loop

--- Practice/calculateThePhonePrice.js
+++ Practice/calculateThePhonePrice.js
@@ -9,6 +9,7 @@ if (bankAmount < phoneCost) {
 }
 
 // Example of "for loop"
+    // Начало   // Условие  //Шаг
 for (let step = 0; step < 5; step++) {
     // Runs 5 times, with values of step 0 through 4.
     console.log('Walking east one step');
@@ -58,3 +59,5 @@ for (let step = 0; step < 5; step++) {
   }
   // you can't afford this purchase
 
+
+  
